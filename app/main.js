5205aaaca8693ce7fb25e05d47c50abf38e9c6e9
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-21 18:57:39 +0200

    File structure for components were added

--- app/main.js
+++ app/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello World!!!</div>;
+};
+
+let container = document.querySelector('[data-component="react"]');
+ReactDOM.render(<App />, container);
