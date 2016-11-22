501cf408e0e9a1d767df83a0e92cfee6135d6843
Be-Smart <serhii.vladymyrov@gmail.com>
2016-11-22 19:21:58 +0200

    Click event was added

--- app/components/video_list_item/index.js
+++ app/components/video_list_item/index.js
@@ -6,8 +6,9 @@ const VideoListItem = (props) => {
   let videoTitle = props.video.snippet.title;
   let imgUrl = props.video.snippet.thumbnails.default.url;
 
+
   return (
-    <li>
+    <li onClick={ () => props.onVideoSelect(props.video) }>
       <div>
         <img src={imgUrl} />
         <span>{videoTitle}</span>
