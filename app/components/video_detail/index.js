949ce31c2557d309aa04f27eee722f70b4b41c49
Be-Smart <serhii.vladymyrov@gmail.com>
2016-12-10 20:35:21 +0200

    Add classNames for elements

--- app/components/video_detail/index.js
+++ app/components/video_detail/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import './video_detail.sass';
 
 const VideoDetail = (props) => {
   if (!props.video) {
@@ -13,14 +14,13 @@ const VideoDetail = (props) => {
   let url = `https://www.youtube.com/embed/${videoId}`;
 
   return (
-    <div>
-      <div>
+    <div className='video'>
+      <div className='video__player'>
         <iframe src={url}></iframe>
       </div>
-      <div>
-        <span>{videoTitle}</span>
-        <br />
-        <span>{videoDescrip}</span>
+      <div className='video__detail'>
+        <h2 className='video__title'>{videoTitle}</h2>
+        <p className='video__description'>{videoDescrip}</p>
       </div>
     </div>
   );
