4e335b13fbf4cc2c11450f4b70b2b69a74b3a375
Be-Smart <serhii.vladymyrov@gmail.com>
2016-12-11 00:30:28 +0200

    Add classNames HTML elements

--- app/components/video_list_item/index.js
+++ app/components/video_list_item/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 import React from 'react';
+import './items.sass';
 
 const VideoListItem = (props) => {
   let videoTitle = props.video.snippet.title;
   let imgUrl = props.video.snippet.thumbnails.default.url;
 
-
   return (
-    <li onClick={ () => props.onVideoSelect(props.video) }>
-      <div>
+    <li className='video-list__item' onClick={ () => props.onVideoSelect(props.video) }>
+      <div className='video-list__img'>
         <img src={imgUrl} />
-        <span>{videoTitle}</span>
       </div>
+      <span className='video-list__title'>{videoTitle}</span>
     </li>
   );
 };
